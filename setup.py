155fe29d130dbf2a761dcca707fa3e15bf9e089c
Maedbh King <mking86@uwo.ca>
2020-10-29 16:46:20 -0700

    added data transfer

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Investigating social prediction using eye tracking and behavioral performance',
+    entry_points={
+    'console_scripts': [
+        'transfer-behavior-from-savio=analysis_code.data.data_transfer:behavior_from_savio',
+        ]
+    },
     author='Shannon Lee and Maedbh King',
     license='MIT',
 )
