0d69069eee8742307142fd94f6ac852bf9401d8e
Shannon Lee <shannonlee@Shannons-MacBook-Air.local>
2021-05-03 18:31:13 -0700

    analysis

--- action_prediction/constants.py
+++ action_prediction/constants.py
@@ -7,12 +7,17 @@ Constants and defaults for running and visualizing connectivity models for the c
 @author: Maedbh King
 """
 
-subj_id = {'sON':'sub-01', 'sEA':'sub-02', 
-                'sAA1':'sub-03', 'sHU':'sub-04', 
-                'sEH': 'sub-05', 'sSE': 'sub-06', 
-                'sEO': 'sub-07', 'sAA': 'sub-08', 
-                'sEE': 'sub-09', 'sML': 'sub-10',
-                'sIU1': 'sub-11', 'sIU': 'sub-12'}
+subj_id =  {'sON':'sub-01', 'sEA':'sub-02', 
+            'sAA1':'sub-03', 'sHU':'sub-04', 
+            'sEH': 'sub-05', 'sSE': 'sub-06', 
+            'sEO': 'sub-07', 'sAA': 'sub-08', 
+            'sEE': 'sub-09', 'sML': 'sub-10',
+            'sIU1': 'sub-11', 'sIU': 'sub-12',
+            'sRH': 'sub-13', 'sAA2': 'sub-14',
+            'sAE': 'sub-15', 'sNH': 'sub-16',
+            'sAO1': 'sub-17', 'sAO': 'sub-18',
+            'sOA': 'sub-19', 'sLG': 'sub-20',
+            'sRO': 'sub-21', 'sEI': 'sub-22'}
 behavioral_sessions = {'ses-1':[1,2,3,4,5,6,7], 'ses-2':[8,9,10,11,12,13,14]}
 tasks = ['semantic_prediction', 'visual_search', 'action_observation', 'social_prediction', 'n_back']
 
